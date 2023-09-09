edf666f04be3e795874b18766ea39f61b6d23418
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-09-08 18:52:21 -0700

    fixed token creation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,36 @@
 const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {},
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
   Mutation: {
-    login: async (parent, args) => {},
+    login: async (parent, args) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      if (!user) {
-        return;
-      }
       const token = signToken(user);
       return { token, user };
     },
